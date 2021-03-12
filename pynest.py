f3115f309ccbef832f19a376301dd556114fcb91
Wahidur Rahman <rahmanw@hotmail.co.uk>
2021-03-12 08:48:23 +0000

    updating repo

--- pynest.py
+++ pynest.py
@@ -19,7 +19,7 @@ class Sampler:
         ndim: int,
         sample_method:str = 'mcmc',
         initial_points: int = 400,
-        evidence_tolerance:float = 0.0001
+        evidence_tolerance:float = 0.1
         ):
 
 
@@ -30,7 +30,7 @@ class Sampler:
         self.intial_points = initial_points
         self.evidence_tolerance = evidence_tolerance
         
-        self.evidence = 0
+        self.log_evidence = 0
         self.information = 0
         self.posterior = []
         self.step_size = 0.001
@@ -60,10 +60,8 @@ class Sampler:
         #Steps to sampling
         #Step 1 - Initial sample  of points
         unit_cubes = [generate_cube(self.ndim) for i in range(self.intial_points)]
-        print('unit cube', unit_cubes[0])
         prior_cubes = [self.prior(cube) for cube in copy.deepcopy(unit_cubes)]
-        print(unit_cubes[0],prior_cubes[0])
-
+        print('cube', unit_cubes[0],prior_cubes[0])
 
         X_count = 1
         X = np.exp(-X_count/self.intial_points)
@@ -73,16 +71,20 @@ class Sampler:
         self.likelihood_output_chain.append(0)
 
 
-
         #Step 2 - Compute Likelihoods
         self.likelihood_live = np.array([self.log_likelihood(cube) for cube in copy.deepcopy(prior_cubes)])
+        #print(self.likelihood_live)
         l_max = max(self.likelihood_live)
-        print(l_max,l_max*(X_prev - X) )
+        print(l_max,np.log(X_prev - X),l_max + np.log(X_prev - X) )
+        print(np.exp(l_max),X_prev - X,np.exp(l_max)*(X_prev - X))
         #Evidence acurracy tolerance
-        while l_max*(X_prev - X) > self.evidence_tolerance:
+        while l_max + np.log(X_prev - X) > self.evidence_tolerance:
             #Step 3 - Discard lowest likelihood
             min_loc = np.where(self.likelihood_live == min(self.likelihood_live))[0][0]
             likelihood_lower_bound = self.likelihood_live[min_loc]
+            
+            self.log_evidence +=  likelihood_lower_bound + np.log(X_prev - X)
+
             unit_prior_lower_bound = unit_cubes[min_loc]
             prior_lower_bound = prior_cubes[min_loc]
             
@@ -92,14 +94,16 @@ class Sampler:
 
             #Step 4 - replace discarded point from samples from prior
             self.step_size = self.step_size * 0.99 # geometric shrinking of step size
-            new_unit_sample = mcmc_proposal(unit_prior_lower_bound, self.step_size)
+            new_unit_sample = mcmc_proposal(unit_prior_lower_bound)
             new_prior_sample = self.prior(copy.deepcopy(new_unit_sample))
+            #print('new',new_unit_sample,new_prior_sample)
             new_likelihood_val = self.log_likelihood(new_prior_sample)
 
             while new_likelihood_val < likelihood_lower_bound:
-                print('sampling...')
-                new_unit_sample = mcmc_proposal(unit_prior_lower_bound, self.step_size)
+                #print('sampling...')
+                new_unit_sample = mcmc_proposal(unit_prior_lower_bound)
                 new_prior_sample = self.prior(copy.deepcopy(new_unit_sample))
+                #print('new',new_unit_sample,new_prior_sample)
                 new_likelihood_val = self.log_likelihood(new_prior_sample)
 
             #overwrite the old discarded sample
@@ -117,7 +121,20 @@ class Sampler:
             self.likelihood_live[min_loc] = self.log_likelihood(new_prior_sample)
 
         #Step 6 - if terminating, compute posterior, evidence and information
-        print(len(self.X_list))
+        #Add up remaining live points
+
+        self.likelihood_live = sorted(self.likelihood_live)
+        print(self.likelihood_live)
+
+        for likval in self.likelihood_live:
+            #log(w*lik) = log(w) + log(lik)
+            self.log_evidence += np.log(X_prev - X) + likval
+            #update X and X_prev
+            X_prev = X
+            X_count += 1
+            X = np.exp(-X_count/self.intial_points)
+
+        print('Log Evidence:',self.log_evidence)
         return
 
     def return_chains():
--- samplers.py
+++ samplers.py
@@ -3,13 +3,16 @@ import pandas as pd
 import typing as tp
 import random
 
-def mcmc_proposal(starting_point:tp.List[float],step_size:float):
+mcmc_scale = 0.1
+def mcmc_proposal(starting_point:tp.List[float]):
     rand_int = random.randint(0,len(starting_point)-1)
-    sample = starting_point[rand_int] + random.uniform(0,1)*step_size
-    #print('proposing a new point')
+    sample = np.random.normal(loc = starting_point[rand_int] , scale = mcmc_scale)
+    #sample = starting_point[rand_int] + step
+
     while sample > 1 or sample <0:
-        sample = starting_point[rand_int] + random.uniform(0,1)*step_size
-        #print(starting_point[rand_int],sample)
+        sample = np.random.normal(loc = starting_point[rand_int] , scale = mcmc_scale)
+        #sample = starting_point[rand_int] + step
+        print('resampling',sample)
+
     starting_point[rand_int] = sample
-    #print('proposal',sample)
     return starting_point
